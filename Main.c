6ce474fa5cd2348a3034fbc6db839767b9020141
Alvaro Camacho <alvaro.camacho2893@gmail.com>
2019-10-09 23:17:35 -0600

    adding calculate pi

--- Main.c
+++ Main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <setjmp.h>
 #include <stdlib.h>
+#include <math.h>
 #define PROCESSES_AVAILABLE 5
 #define work_unit_size 50
 int Work_by_Process[5]={25,10,5,15,20};
@@ -11,7 +12,7 @@ int sorted_copy[PROCESSES_AVAILABLE];
 /*
 The previous definitions need to be configured by process so that each process has different amout of work
 The previous need to be defined by text file
-The prvious need to be set dinamically to N process 
+The prvious need to be set dinamically to N process
 */
 sigjmp_buf mark[PROCESSES_AVAILABLE];
 void FCFS_Scheduler_Execution();
@@ -22,7 +23,8 @@ void SJF_Scheduler_Execution();
 void SJF_Scheduler_Selection();
 void SJF_Scheduler_aux();
 void SJF_Scheduler();
-float Calculate_Pi();
+//float Calculate_Pi();
+long double Calculate_Pi();
 void sort();
 int main()
 {
@@ -43,11 +45,19 @@ void FCFS_Scheduler_aux(){
 		return;
 	}
 	FCFS_Scheduler_Execution();
-		
+
 }
 void FCFS_Scheduler_Execution()
 {
-	Calculate_Pi();
+	//Calculate_Pi();
+
+	unsigned int n = 1000000000;
+	long double result = 0;
+
+	result = Calculate_Pi(n);
+
+	printf("terminos = %u, aproximacion = %Lf\n", n, result );
+
 	printf("Current Thread %d\n",curThread);
 	siglongjmp(mark[curThread], -1);
 }
@@ -61,13 +71,22 @@ void FCFS_Scheduler_Selection()
 	curThread++;
 }
 
-float Calculate_Pi(){
+/*float Calculate_Pi(){
 
 	int i,j;
 	float result=1.0;
-	printf("Pi result is %f\n",2*result);	
+	printf("Pi result is %f\n",2*result);
 	//Needs clarification since there are singificant issue if we want to calculate arcsin taylor series
 	return 2*result;
+}*/
+
+long double Calculate_Pi(unsigned int terminos) {
+    long double ans = 4;
+
+    for (unsigned int i = 1; i <= terminos; i++) {
+      ans += (4*pow(-1,i))/(2*i+1);
+    }
+    return ans;
 }
 
 
@@ -90,11 +109,19 @@ void SJF_Scheduler_aux(){
 		return;
 	}
 	SJF_Scheduler_Execution();
-		
+
 }
 void SJF_Scheduler_Execution()
 {
-	Calculate_Pi();
+	//Calculate_Pi();
+
+	unsigned int n = 1000000000;
+	long double result = 0;
+
+	result = Calculate_Pi(n);
+
+	printf("terminos = %u, aproximacion = %Lf\n", n, result );
+
 	printf("Current Thread %d\n",curThread);
 	siglongjmp(mark[curThread], -1);
 }
@@ -112,14 +139,14 @@ void SJF_Scheduler_Selection()
 /*
 Call to srot algorithm
 */
- 
-// Driver program to test above functions 
-void sort() 
-{ 
+
+// Driver program to test above functions
+void sort()
+{
 	memcpy(sorted_copy, Work_by_Process, sizeof(sorted_copy));
-	int n = sizeof(sorted_copy)/sizeof(sorted_copy[0]); 
-	quickSort(sorted_copy, 0, n-1); 
-	printf("Sorted array: n"); 
-	printArray(sorted_copy, n); 
-	printArray(Work_by_Process, n); 
-} 
+	int n = sizeof(sorted_copy)/sizeof(sorted_copy[0]);
+	quickSort(sorted_copy, 0, n-1);
+	printf("Sorted array: n");
+	printArray(sorted_copy, n);
+	printArray(Work_by_Process, n);
+}
